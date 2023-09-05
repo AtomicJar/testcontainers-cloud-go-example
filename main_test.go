b4b8ec3ff6c7bb5d1553fe4c8e7be8a1f107b26d
Eddú Meléndez <eddu.melendez@gmail.com>
2023-09-05 12:48:38 -0600

    Assert query result

--- main_test.go
+++ main_test.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
+	"gotest.tools/v3/assert"
 	"path/filepath"
 	"strings"
 	"testing"
 	"time"
 
+	_ "github.com/lib/pq"
 	"github.com/stretchr/testify/require"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/postgres"
@@ -29,7 +32,24 @@ func TestTestcontainersCloud(t *testing.T) {
 				WithOccurrence(2).WithStartupTimeout(10*time.Second)),
 	)
 	require.NoError(t, err)
-	defer c.Terminate(ctx)
+	t.Cleanup(func() {
+		if err := c.Terminate(ctx); err != nil {
+			t.Fatalf("failed to terminate container: %s", err)
+		}
+	})
+
+	cs, _ := c.ConnectionString(ctx, "sslmode=disable")
+	db, err := sql.Open("postgres", cs)
+	defer db.Close()
+
+	var numberOfGuides int
+	result, err := db.Query("SELECT COUNT(*) FROM guides")
+	defer result.Close()
+
+	result.Next()
+	result.Scan(&numberOfGuides)
+	require.NoError(t, err)
+	assert.Equal(t, numberOfGuides, 6)
 
 	dockerClient, err := testcontainers.NewDockerClient()
 	require.NoError(t, err)
